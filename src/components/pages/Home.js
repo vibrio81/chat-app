dde7f99c42f477317c676f7392334630e3e5b205
Lila Stone-Porter <74431023+vibrio81@users.noreply.github.com>
2020-12-31 07:58:09 -0600

    incomplete first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Header from '../Header';
+import Footer from '../footer';
+import { Link } from 'react-router-dom';
+
+ class HomePage extends Component {
+  render() {
+    return (
+      <div className="home">
+        <Header></Header>
+        <section>
+          <div className="jumbotron jumbotron-fluid py-5">
+            <div className="container text-center py-5">
+              <h1 className="display-4">Welcome to Don't Call Me!</h1>
+              <p className="lead">No one wants to talk to you.</p>
+              <div className="mt-4">
+                <Link className="btn btn-primary px-5 mr-3" to="/signup">Create New Account</Link>
+                <Link className="btn px-5" to="/login">Login to Your Account</Link>
+              </div>
+            </div>
+          </div>
+        </section>
+        <Footer></Footer>
+      </div>
+    )
+  }
+}
+export default HomePage;
---
+++ src/components/pages/Login.js
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { signin, signInWithGoogle, signInWithGitHub } from "../helpers/auth";
+
+ class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+      email: "",
+      password: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.googleSignIn = this.googleSignIn.bind(this);
+    this.githubSignIn = this.githubSignIn.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  keyPress(event){
+    if(event.keyCode == 13){
+       console.log('value', event.target.value);
+       
+    }
+ }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ error: "" });
+    try {
+      await signin(this.state.email, this.state.password);
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  }
+
+  async googleSignIn() {
+    try {
+      await signInWithGoogle();
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  }
+
+  async githubSignIn() {
+    try {
+      await signInWithGitHub();
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <form
+          className="mt-5 py-5 px-5"
+          autoComplete="on"
+          onSubmit={this.handleSubmit}
+          onKeyDown={this.keyPress}
+        >
+          <h1>
+            Login to
+            <Link className="title ml-2" to="/">
+              <br></br>Don't Call Me!
+            </Link>
+          </h1>
+          <p className="lead">
+            Fill in the form below to login to your account.
+          </p>
+          <div className="form-group">
+            <input
+              className="form-control"
+              placeholder="Email"
+              name="email"
+              type="email"
+              onChange={this.handleChange}
+              onKeyDown={this.keyPress}
+              value={this.state.email}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              placeholder="Password"
+              name="password"
+              onChange={this.handleChange}
+              onKeyDown={this.keyPress}
+              value={this.state.password}
+              type="password"
+            />
+          </div>
+          <div className="form-group">
+            {this.state.error ? (
+              <p className="text-danger">{this.state.error}</p>
+            ) : null}
+            <button className="btn btn-primary px-5" type="submit">Login</button>
+          </div>
+          <p>You can also log in with any of these services</p>
+          <button className="btn btn-danger mr-2" type="button" onClick={this.googleSignIn}>
+            Sign in with Google
+          </button>
+          <button className="btn btn-secondary" type="button" onClick={this.githubSignIn}>
+            Sign in with GitHub
+          </button>
+          <hr />
+          <p>
+            Don't have an account? <Link to="/signup">Sign up</Link>
+          </p>
+        </form>
+
+      </div>
+    );
+  }
+}
+export default Login;
